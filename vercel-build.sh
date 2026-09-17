9692ee1966eb2d6008b500825a9f770bc9b48e4c
Adhiraj Jain <your-vercel-email@example.com>
2026-09-17 23:37:17 +0530

    auth fixed

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,13 +3,13 @@
 # 1. Download Flutter
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 
-# 2. Add to PATH
-export PATH="$PATH:`pwd`/flutter/bin"
+# 2. Add to PATH (PREPEND to ensure we use this version)
+export PATH="`pwd`/flutter/bin:$PATH"
 
 # 3. Build for Web
 flutter config --enable-web
 flutter pub get
-flutter build web --release --web-renderer html
+flutter build web --release
 
 # 4. Move build to a folder Vercel can see easily if needed
 # (Optional, but helps if you set output directory to 'build/web')
