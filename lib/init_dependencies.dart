87f43c4cfb50d4fdd26a46e2ca45778de911a362
Adhiraj Jain <your-vercel-email@example.com>
2026-09-18 15:47:56 +0530

    Apis hidden and secured all the data

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -26,8 +26,7 @@ Future<void> initDependencies() async {
   serviceLocator.registerLazySingleton(() => FirebaseAuth.instance);
   serviceLocator.registerLazySingleton(() => FirebaseFirestore.instance);
   serviceLocator.registerLazySingleton(() => GoogleSignIn(
-        clientId:
-            '745060405583-fulnte2av4ooagafe1gfdo3rvjkqnpgd.apps.googleusercontent.com',
+        clientId: const String.fromEnvironment('GOOGLE_CLIENT_ID'),
       ));
 
   // Auth Feature
